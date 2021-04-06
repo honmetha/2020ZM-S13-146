16bfa03ccc14736a588c7362c3ae057f325c48e3
Hon <dennis.mejen@gmail.com>
2021-04-06 15:13:32 +0700

    [Added] 18 What is prototype

--- 18 - What is prototype/start.js
+++ 18 - What is prototype/start.js
@@ -1,8 +1,30 @@
 (() => {
   // เริ่มเขียนโค้ด
 
-  // 1. Class vs Prototype 
+  // 1. Class vs Prototype
+  // class Person {}
+
+  // const varayut = new Person();
+  // console.log(varayut);
+
   // 2. What's prototype?
+  // const name = "varayut";
+  // console.log(name);
+
+  // const arr = [];
+  // console.log(arr.__proto__);
+
   // 3. Prototype chain
+  // const name = "varayut";
+  // console.log(name.__proto__);
+  // console.log(name.toLocaleString());
+
   // 4. Extend a prototype
+  const name = "varayut";
+  function sayHello(val) {
+    console.log(`Hello ${val}`);
+  }
+  String.prototype.sayHello = sayHello;
+  console.log(name.__proto__);
+  name.sayHello("world");
 })();
